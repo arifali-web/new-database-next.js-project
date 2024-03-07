c5be2463a9639f3495d1f9c3c501dbad9f654098
arifali-web <arifgopang235@gmail.com>
2024-03-07 22:28:30 +0500

    update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,20 @@
 import { AppProps } from "next/app";
+import { SessionProvider } from 'next-auth/react';
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <Component {...pageProps} />
+    <SessionProvider session={pageProps.session}>
+      <Component {...pageProps} />
+    </SessionProvider>
   );
 };
 
 export default App;
+
+// const App = ({ Component, pageProps }: AppProps) => {
+//   return (
+//     <Component {...pageProps} />
+//   );
+// };
+
+// export default App;
